5a457284242ef1a6944636dc7401962a18b1d39e
Alex Guy <spaunfrom@gmail.com>
2018-06-06 00:47:05 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".main-slider").owlCarousel({
+    autoplay: true,
+  	items: 1,
+  	loop: true,
+  	touchDrag: true,
+  	nav: true,
+  	navText: ['', ''],
+  	dots: true
+  });
+});
