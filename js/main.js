65e0706ae45e8e67fa08dbbf501cb1cc604b9243
Alex Guy <spaunfrom@gmail.com>
2018-06-07 00:12:03 +0300

    add scroll btn

--- js/main.js
+++ js/main.js
@@ -10,5 +10,14 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function() {
+    $(".slide-section").click(function(e){
 
+    	var linkHref = $(this).attr('href');
+    	$('html, body').animate({
+    		scrollTop: $(linkHref).offset().top
+    	}, 700);
 
+        e.preventDefault();
+    });
+});
