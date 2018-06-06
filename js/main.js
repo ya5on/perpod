2c76eae5a49bbc3d5bec65e414c2550c49c11f5d
Alex Guy <spaunfrom@gmail.com>
2018-06-06 15:54:26 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-$(document).ready(function(){
-  $(".main-slider").owlCarousel({
-    autoplay: true,
-  	items: 1,
-  	loop: true,
-  	touchDrag: true,
-  	nav: true,
-  	navText: ['', ''],
-  	dots: true
-  });
+$(document).ready(function() {
+    $(".main-slider").owlCarousel({
+        autoplay: true,
+        items: 1,
+        loop: true,
+        touchDrag: true,
+        nav: true,
+        navText: ['', ''],
+        dots: true
+    });
 });
+
+
+
